82d86892ff4408718f0e6d033d543dcec009d689
Mataya Duncan <matayad@uark.edu>
2024-03-11 08:42:19 -0500

    adding the dif +

--- hello-world.py
+++ hello-world.py
@@ -4,6 +4,9 @@
 print('hello world', end= " ")
 print('hello, Beyonce')
 
+print('1' + '2' + '3')
+print(1+2+3)
+
 print('enter name of best friend:')
 bestfriend = input()
 print('my best friend is', bestfriend)
