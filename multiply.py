5388dce0c29958d9acede5640bd619eea4257c4d
Mataya Duncan <matayad@uark.edu>
2024-04-02 16:17:02 -0500

    adding the updated multiplication script

--- multiply.py
+++ multiply.py
@@ -1,16 +1,33 @@
 #! /usr/bin/env python3
 
-x = int(input("Enter a number: "))
-n = range(1, (x+1), 1)
-longest = x * x
-#print(longest)
-width = len(str(longest))
-truewidth = width + 1
-#print(truewidth)
+def get_input():
+    #ask for the size of the multiplication matrix
+    x = int(input("Enter a number: "))
+    # specify range for loop
+    n = range(1, (x+1), 1)
 
-for row in n:
-    for col in n:
-        print(('{:>{truewidth}}'.format(row*col, truewidth = truewidth)), end = "")
-    print()
+    return x, n
 
-#{:>{length}s}'.format(s, length=length)
+def print_table(x, n):
+
+    # calculate how many spaces we'll need for each cell matrix
+    truewidth = len(str(x * x)) + 1
+
+    #nested for loop to calculate row * col
+    for row in n:
+        for col in n:
+            # nice formatted print statement for each cell
+            print(('{:>{truewidth}}'.format(row*col, truewidth = truewidth)), end = "")
+        #print a newline character at the end of each row
+        print()
+
+def main():
+    input_num, width = get_input()
+
+    print_table(input_num, width)
+
+
+#set the environment for this script
+#is it main, or is this a module being called by another script?
+if __name__ == '__main__':
+    main()
